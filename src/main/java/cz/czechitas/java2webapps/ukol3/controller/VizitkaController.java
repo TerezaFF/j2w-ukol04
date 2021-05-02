6bf8bb99e72d5406124a4c59f20a8ff242512076
Tereza Faustova <tereza.faustova@capgemini.com>
2021-05-02 19:04:44 +0200

    Ctvrty ukol

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -3,6 +3,7 @@ package cz.czechitas.java2webapps.ukol3.controller;
 import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -99,4 +100,23 @@ public class VizitkaController {
     result.addObject("vizitka", seznamVizitek.get(id));
     return result;
   }
+
+  @GetMapping(value = "/nova")
+  public ModelAndView nova() {
+    ModelAndView result = new ModelAndView("nova");
+    return result;
+  }
+
+  @PostMapping(value = "/nova", params = {"jmeno", "firma", "ulice", "obecPsc", "email", "telefon", "web"})
+  public String add(Vizitka vizitka) {
+    seznamVizitek.add(vizitka);
+    return "redirect:/";
+  }
+
+  @PostMapping(value = "/detail", params = {"id"})
+  public String delete(int id) {
+    seznamVizitek.remove(id);
+    return "redirect:/";
+  }
+
 }
